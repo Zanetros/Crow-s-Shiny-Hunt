89bae9e7a3926295377d844858d9f40473805bd6
Zanetros <73186724+Zanetros@users.noreply.github.com>
2024-07-07 00:46:45 -0300

    animação da maça

--- Assets/Scripts/Enviroment/Apple.cs
+++ Assets/Scripts/Enviroment/Apple.cs
@@ -6,12 +6,22 @@ using UnityEngine;
 public class Apple : MonoBehaviour
 {
     public Rigidbody rb;
+    public string player = "Player";
+    public string fall = "fall";
+    public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag(player))
         {
-            rb.useGravity = true;
+            StartCoroutine(WaitForApple());
         }
+    }
+
+    public IEnumerator WaitForApple()
+    {
+        animator.SetBool(fall, true);
+        yield return new WaitForSeconds(0.8f);
+        rb.useGravity = true;
     }
 }
